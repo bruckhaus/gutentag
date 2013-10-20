8e8b201c15c7f150cb006ec1553df4f985c796c1
Tilmann Bruckhaus <Tilmann.Bruckhaus@gmail.com>
2013-10-20 11:27:37 -0700

    incomplete: add tests

---
+++ gutenTag/tagger.py
@@ -0,0 +1,84 @@
+class Tagger:
+    import nltk
+
+    def __init__(self):
+        sentence_re = r'''(?x)      # set flag to allow verbose regexps
+              ([A-Z])(\.[A-Z])+\.?  # abbreviations, e.g. U.S.A.
+            | \w+(-\w+)*            # words with optional internal hyphens
+            | \$?\d+(\.\d+)?%?      # currency and percentages, e.g. $12.40, 82%
+            | \.\.\.                # ellipsis
+            | [][.,;"'?():-_`]      # these are separate tokens
+        '''
+
+        lemmatizer = nltk.WordNetLemmatizer()
+        stemmer = nltk.stem.porter.PorterStemmer()
+
+        #Taken from Su Nam Kim Paper...
+        grammar = r"""
+            NBAR:
+                {<NN.*|JJ>*<NN.*>}  # Nouns and Adjectives, terminated with Nouns
+
+            NP:
+                {<NBAR>}
+                {<NBAR><IN><NBAR>}  # Above, connected with in/of/etc...
+        """
+        chunker = nltk.RegexpParser(grammar)
+
+
+    @property
+    def tag(text):
+        """
+        Tag a text.
+        """
+
+        # Used when tokenizing words
+        tokens = nltk.regexp_tokenize(text, sentence_re)
+        pos_tokens = nltk.tag.pos_tag(tokens)
+
+
+
+        print "\npos_tag():"
+        print pos_tokens
+
+        tree = chunker.parse(pos_tokens)
+
+        from nltk.corpus import stopwords
+
+        def leaves(tree):
+            """Finds NP (nounphrase) leaf nodes of a chunk tree."""
+            for subtree in tree.subtrees(filter=lambda t: t.node == 'NP'):
+                yield subtree.leaves()
+
+
+        def normalise(word):
+            """Normalises words to lowercase and stems and lemmatizes it."""
+            word = word.lower()
+            word = stemmer.stem_word(word)
+            word = lemmatizer.lemmatize(word)
+            return word
+
+
+        def acceptable_word(word):
+            """Checks conditions for acceptable word: length, stopword."""
+            accepted = bool(2 <= len(word) <= 40
+            and word.lower() not in stopwords)
+            return accepted
+
+
+        def get_terms(tree):
+            for leaf in leaves(tree):
+                term = [normalise(w) for w, t in leaf if acceptable_word(w)]
+                yield term
+
+        stopwords = stopwords.words('english')
+
+        terms = get_terms(tree)
+
+        print "\nterms:"
+        for term in terms:
+            for word in term:
+                print word,
+            print
+
+
+
---
+++ tests/test_tag.py
@@ -0,0 +1,60 @@
+import random
+import unittest
+
+
+class TestTagger(unittest.TestCase):
+
+    def test_tag(self):
+
+        text = """The Buddha, the Godhead, resides quite as comfortably in the circuits of a digital
+        computer or the gears of a cycle transmission as he does at the top of a mountain
+        or in the petals of a flower. To think otherwise is to demean the Buddha...which is
+        to demean oneself."""
+
+        self.assertTrue(tag(text) == 'foo')
+
+        #text = """Marketaire.com  Founder - Internet marketing strategies & tactics. SEO, SEM, SMM. Let's do this.
+        #Toronto, Ontario, Canada . marketaire.com"""
+        #
+        #text = """SEM SEO SMM PPC Performance Suchmaschinen Marketing sponsored listings affiliate adwords paid
+        #search social ROI tracking; twitterbot; follow @ressmann my human"""
+        #
+        #text = """#Technology, science, self-development, organizational methods, #SM, #SMM.
+        # Following #business trends and developments.
+        # Top 1% Kred."""
+        #
+        #text = """["San Franciscan, yogi, rubyist, entrepreneur, @RubyRogues panelist, organizer of Golden Gate RubyConf", "Rubyist, motorcycle racer, cheapskate.\r\nDeveloper of Sidekiq http://t.co/ngmJl4Giae\r\nTech guy at @TheClymb", "GitHub Cofounder"]"""
+        #
+        #text = """San Franciscan, yogi, rubyist, entrepreneur, @RubyRogues panelist, organizer of Golden Gate RubyConf"""
+        #
+        #text = """Rubyist, motorcycle racer, cheapskate.\r\nDeveloper of Sidekiq http://t.co/ngmJl4Giae\r\nTech guy at @TheClymb"""
+        #
+        #text = """GitHub Cofounder"""
+
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.seq = range(10)
+
+    def test_shuffle(self):
+        # make sure the shuffled sequence does not lose any elements
+        random.shuffle(self.seq)
+        self.seq.sort()
+        self.assertEqual(self.seq, range(10))
+
+        # should raise an exception for an immutable sequence
+        self.assertRaises(TypeError, random.shuffle, (1,2,3))
+
+    def test_choice(self):
+        element = random.choice(self.seq)
+        self.assertTrue(element in self.seq)
+
+    def test_sample(self):
+        with self.assertRaises(ValueError):
+            random.sample(self.seq, 20)
+        for element in random.sample(self.seq, 5):
+            self.assertTrue(element in self.seq)
+
+if __name__ == '__main__':
+    unittest.main()
